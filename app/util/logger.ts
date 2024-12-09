e4322aff5eb8c731ed1682f2d9488bc9b8fc1cb3
Brent Kao(KO) <109659089+brentkao@users.noreply.github.com>
2024-12-10 02:30:32 +0800

    Initial commit

---
+++ app/util/logger.ts
@@ -0,0 +1,48 @@
+import { env } from "../../env";
+
+interface LoggerOptions {
+  data?: any;
+  depth?: number | null | undefined;
+  retainLogs?: boolean;
+  msg?: string;
+  pointer?: boolean;
+}
+interface LoggerTableOptions {
+  data?: any;
+  retainLogs?: boolean;
+  msg?: string;
+  pointer?: boolean;
+}
+
+const isProduction = env.NODE_ENV === "production";
+
+export function logger(
+  tag: string,
+  {
+    data,
+    depth = 2,
+    retainLogs = false,
+    msg = "",
+    pointer = false,
+  }: LoggerOptions = {}
+) {
+  if (!retainLogs && isProduction) return;
+  const fullTag = pointer ? `➫ [${tag}]` : `[${tag}]`;
+  console.log(fullTag, msg);
+  if (data) console.dir(data, { depth });
+}
+
+export function loggerTable(
+  tag: string,
+  {
+    data,
+    retainLogs = false,
+    msg = "",
+    pointer = false,
+  }: LoggerTableOptions = {}
+) {
+  if (!retainLogs && isProduction) return;
+  const fullTag = pointer ? `➫ [${tag}]` : `[${tag}]`;
+  console.log(fullTag, msg);
+  if (data) console.table(data);
+}
