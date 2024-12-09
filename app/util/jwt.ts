222de8912a8bb4f79366239431d498e4550936e5
Brent Kao(KO) <109659089+brentkao@users.noreply.github.com>
2024-12-10 02:30:32 +0800

    Initialize SMS-every8d

---
+++ app/util/jwt.ts
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import { env } from "../../env";
+
+// 定義一個類型來限定用戶角色的可能值
+export type UserRole = "developer" | "api" | "user";
+
+// 定義JwtPayload接口
+export interface JwtPayload {
+  /** 用戶的唯一標識 */
+  userId: string;
+  /** 用戶的角色 */
+  role: UserRole;
+  // 根據需要可以添加更多的屬性
+}
+
+// 定義一個接口來描述JWT payload的結構
+export function generateLoginJwt(payload: JwtPayload) {
+  const secretKey = env.JWT_SECRET;
+  const token = jwt.sign(payload, secretKey, { expiresIn: "24h" });
+  return token;
+}
